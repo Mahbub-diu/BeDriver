5492cc7724ff9aa4e3d7102466d1b7917b57c08a
Mahbub-diu <mahbub.mediasoft@gmail.com>
2025-01-15 16:26:56 +0600

    update fleet

---
+++ test.php
@@ -0,0 +1,196 @@
+  <!-- officina slider start from here  -->
+  <section class="home-slider-main officina-main transporti-slider-main">
+
+      <div class="slider-content-box">
+          <div class="centerd">
+              <h5>NON UN SEMPLICE CARROATTREZZI</h5>
+              <h1>Servizio di trasporto con mezzi coperti</h1>
+              <h6> Il tuo partner di fiducia per trasportare la tua auto con privacy garantita</h6>
+              <div class="card-grid-parent">
+                  <div class="single-car-box">
+                      <div class="icon">
+                          <img src="<?php echo get_template_directory_uri();?>/assets/images/transporti/car.png" alt="">
+                      </div>
+                      <div class="title">Auto d’epoca</div>
+                  </div>
+                  <div class="single-car-box">
+                      <div class="icon">
+                          <img src="<?php echo get_template_directory_uri();?>/assets/images/transporti/car.png" alt="">
+                      </div>
+                      <div class="title">Auto racing</div>
+                  </div>
+                  <div class="single-car-box">
+                      <div class="icon">
+                          <img src="<?php echo get_template_directory_uri();?>/assets/images/transporti/car.png" alt="">
+                      </div>
+                      <div class="title">Auto stradali</div>
+                  </div>
+                  <div class="single-car-box">
+                      <div class="icon">
+                          <img src="<?php echo get_template_directory_uri();?>/assets/images/transporti/car.png" alt="">
+                      </div>
+                      <div class="title">Auto di lusso</div>
+                  </div>
+
+              </div>
+          </div>
+      </div>
+
+      <div class="slider-contact">
+          <p>contattaci</p>
+          <a href="#" target="_blank">
+              <img src="<?php echo get_template_directory_uri();?>/assets/images/icon/Facebook.svg" alt="">
+          </a>
+          <a href="#" target="_blank">
+              <img src="<?php echo get_template_directory_uri();?>/assets/images/icon/LinkedIn.svg" alt="">
+          </a>
+          <a href="#" target="_blank">
+              <img src="<?php echo get_template_directory_uri();?>/assets/images/icon/LinkedIn.svg" alt="">
+          </a>
+          <a href="#" target="_blank">
+              <img src="<?php echo get_template_directory_uri();?>/assets/images/icon/Tel.svg" alt="">
+          </a>
+      </div>
+  </section>
+  <!-- officina slider ends here  -->
+
+  <!--be drive garage start from here  -->
+
+  <section class="bdrive-garage-main transport-banner-main"
+      data-background="<?php echo get_template_directory_uri();?>/assets/images/officina/garage_header.png">
+
+  </section>
+
+  <!--be drive garage ends here  -->
+
+  <!--private service first one start from here  -->
+
+  <section class="private-service-main first-service-main py-150">
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                  <div class="title-box">
+
+                      <p> Assicuriamo privacy assoluta nel recupero e nella riconsegna delle vostre auto, <br>
+                          utilizzando un mezzo coperto di nostra proprietà e privo di qualsiasi livrea e <br>
+                          riconoscimento, guidato da personale altamento qualificato.</p>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+  </section>
+
+  <!--private service first ends here  -->
+
+  <!-- perche icon section start  -->
+
+  <section class="perche-icon-main">
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3 col-xl-2">
+                  <div class="icon-wrapper">
+                      <div class="icon">
+                          <img src="<?php echo get_template_directory_uri();?>/assets/images/transporti/1.png" alt="">
+                      </div>
+                      <div class="title">Ritiro e <br> consegna veloce</div>
+                  </div>
+              </div>
+              <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3 col-xl-2">
+                  <div class="icon-wrapper">
+                      <div class="icon">
+                          <img src="<?php echo get_template_directory_uri();?>/assets/images/transporti/2.png" alt="">
+                      </div>
+                      <div class="title">Assistenza <br> continua</div>
+                  </div>
+              </div>
+              <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3 col-xl-2">
+                  <div class="icon-wrapper">
+                      <div class="icon">
+                          <img src="<?php echo get_template_directory_uri();?>/assets/images/transporti/3.png" alt="">
+                      </div>
+                      <div class="title">Carroattrezzi <br> o bilico</div>
+                  </div>
+              </div>
+              <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3 col-xl-2">
+                  <div class="icon-wrapper">
+                      <div class="icon">
+                          <img src="<?php echo get_template_directory_uri();?>/assets/images/transporti/4.png" alt="">
+                      </div>
+                      <div class="title">Trasporto <br> dedicato</div>
+                  </div>
+              </div>
+              <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3 col-xl-2">
+                  <div class="icon-wrapper">
+                      <div class="icon">
+                          <img src="<?php echo get_template_directory_uri();?>/assets/images/transporti/5.png" alt="">
+                      </div>
+                      <div class="title">Ritiro e riconsegna <br> In tutta Europa</div>
+                  </div>
+              </div>
+              <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3 col-xl-2">
+                  <div class="icon-wrapper">
+                      <div class="icon">
+                          <img src="<?php echo get_template_directory_uri();?>/assets/images/transporti/6.png" alt="">
+
+                      </div>
+                      <div class=" title">Driver <br> certiricato
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
+
+  <!-- perche icon section ends -->
+
+  <section class="transportiamo-main"
+      data-background="<?php echo get_template_directory_uri();?>/assets/images/officina/garage_header.png">
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                  <div class="title-box">
+                      <h3>Trasportiamo <br> il tuo mezzo <br> in <span>3 semplici passaggi</span></h3>
+                  </div>
+                  <div class="round-point-parent">
+                      <div class="round-wrapper">
+                          <div class="single-round">
+                              <div class="number">1</div>
+                          </div>
+                          <div class="single-round">
+                              <div class="number">2</div>
+                          </div>
+                          <div class="single-round">
+                              <div class="number">3</div>
+                          </div>
+                      </div>
+                      <span class="single-point">CONTATTACI</span>
+                      <span class="single-point">RICEVI IL PREVENTIVO</span>
+                      <span class="single-point">FISSA L’APPUNTAMENTO</span>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
+
+  <!-- officina contact start  -->
+
+  <section class="officina-contact-main motorsport-contact-main">
+      <div class="container-fluid">
+          <div class="row justify-content-center">
+              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-9">
+                  <div class="contact-flex">
+                      <div class="title-box">
+                          <h5>- CONTACT US -</h5>
+                          <p>Vuoi usufruire dei servizi di officina?</p>
+                      </div>
+                      <div class="btn-box">
+                          <a href="#">contattaci ora</a>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
+
+  <!-- officina contact end -->
